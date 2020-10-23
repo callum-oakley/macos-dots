77b2aca5ea75222d23ea9925d0b38efda66c2967
Callum Oakley <hello@callumoakley.net>
2020-10-23 16:06:40 +0100

    breathing status light

--- .config/ubersicht-widgets/mods.jsx
+++ .config/ubersicht-widgets/mods.jsx
@@ -23,6 +23,16 @@ const white = css`
   color: #fff;
 `
 
+const blackAnimated = css`
+  color: #222;
+  transition: color 5s;
+`
+
+const whiteAnimated = css`
+  color: #fff;
+  transition: color 5s;
+`
+
 export const init = (dispatch) => {
   console.log("starting ws-pipe")
   run(
@@ -33,23 +43,53 @@ export const init = (dispatch) => {
     console.log("establishing websocket")
     const ws = new WebSocket("ws://localhost:13748")
     ws.addEventListener("message", (event) => {
-      dispatch({ output: JSON.parse(event.data) })
+      dispatch({ type: "mods", mods: JSON.parse(event.data) })
     })
   }, 1000)
+
+  setInterval(() => {
+    dispatch({ type: "blink" })
+  }, 5000)
 }
 
-export const render = ({ output: { shift, ctrl, alt, cmd } }) => (
+export const initialState = {
+  mods: {},
+  blink: false,
+}
+
+export const updateState = (event, previousState) => {
+  switch (event.type) {
+    case "mods": {
+      return { ...previousState, mods: event.mods }
+      break
+    }
+    case "blink": {
+      return { ...previousState, blink: !previousState.blink }
+    }
+  }
+}
+
+export const render = ({ mods, blink }) => (
   <pre className={preCss}>
-    <Light key="0" on={shift}></Light>
-    <Light key="1" on={ctrl}></Light>
-    <Light key="2" on={alt}></Light>
-    <Light key="3" on={cmd}></Light>
+    <Light key="0" on={mods.shift} onColor={white} offColor={black}></Light>
+    <Light key="1" on={mods.ctrl} onColor={white} offColor={black}></Light>
+    <Light key="2" on={mods.alt} onColor={white} offColor={black}></Light>
+    <Light key="3" on={mods.cmd} onColor={white} offColor={black}></Light>
     <span key="4"> </span>
-    <Light key="5" on={cmd}></Light>
-    <Light key="6" on={alt}></Light>
-    <Light key="7" on={ctrl}></Light>
-    <Light key="8" on={shift}></Light>
+    <Light
+      key="5"
+      on={blink}
+      onColor={whiteAnimated}
+      offColor={blackAnimated}
+    ></Light>
+    <span key="6"> </span>
+    <Light key="7" on={mods.cmd} onColor={white} offColor={black}></Light>
+    <Light key="8" on={mods.alt} onColor={white} offColor={black}></Light>
+    <Light key="9" on={mods.ctrl} onColor={white} offColor={black}></Light>
+    <Light key="10" on={mods.shift} onColor={white} offColor={black}></Light>
   </pre>
 )
 
-const Light = ({ on }) => <span className={on ? white : black}>·</span>
+const Light = ({ on, onColor, offColor }) => (
+  <span className={on ? onColor : offColor}>·</span>
+)
